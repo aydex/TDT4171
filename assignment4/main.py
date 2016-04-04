ccae124c03edb065c2498d962a64ab2cf6e00c61
Adrian Hundseth <adrian.hundseth@gmail.com>
2016-04-05 01:44:59 +0200

    Added some comments

--- assignment4/main.py
+++ assignment4/main.py
@@ -1,27 +1,32 @@
 def plurality_value(examples):
+    # TODO
     pass
 
 
 def importance(attributes, examples):
+    # TODO
     pass
 
 
 def decision_tree_learning(examples, attributes, parent_examples):
     if len(examples) is 0:
         return plurality_value(parent_examples)
-    elif examples:
+    elif examples:  # Needs amendment
         return examples
     elif len(attributes) is 0:
         return plurality_value(examples)
     else:
-        A = max(importance(attributes, examples))
-        tree = A
-        for v in A:
-            exs = v
-            subtree = decision_tree_learning(exs, attributes-A, examples)
+        A = max(importance(attributes, examples))  # Needs amendment
+        tree = A  # Needs amendment
+        for v in A:  # Needs amendment
+            exs = v  # Needs amendment
+            subtree = decision_tree_learning(exs, attributes - A, examples)
+            # Need to add a branch to tree with label (A = vk) and subtree "subtree" somewhere"
         return True
+
 
 def main():
     pass
+
 
 main()
